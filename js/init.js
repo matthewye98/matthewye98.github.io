6157a4d185124a86877b7647d1e0207a6cb6c3aa
Matt Ye <mye@umd.edu>
2019-01-17 21:01:00 -0500

    initial commit

---
+++ js/init.js
@@ -0,0 +1,12 @@
+(function($){
+	$(function(){
+
+		$('.button-collapse').sideNav();
+		$('.parallax').parallax();
+		$('select').material_select();
+		$('.modal-trigger').leanModal();
+    $( document ).ready(function(){
+      $(".button-collapse").sideNav();
+
+	}); // end of document ready
+})(jQuery); // end of jQuery name space
